1b03131e677dc4765370e2789d54f97fce3c992e
Gideon Geier <s8gigeie@stud.uni-saarland.de>
2023-02-25 19:16:04 +0100

    adapt preprocessor

--- Fuzzer/src/preprocessor.py
+++ Fuzzer/src/preprocessor.py
@@ -3,7 +3,7 @@ import subprocess
 import shutil
 import random
 
-from ISASim.host import isaInput
+from HSCSim.host import hscInput
 from RTLSim.host import rtlInput
 from mutator import simInput, templates, P_M, P_S, P_U, V_U
 
@@ -54,53 +54,10 @@ class rvPreProcessor():
         fd.write('{:016x}:{:04b}\n'.format(epc, val))
         fd.close()
 
-    def process(self, sim_input: simInput, data: list, intr: bool, num_data_sections=6):
-        section_size = len(data) // num_data_sections
-
-        assert data, 'Empty data can not be processed'
-        assert (section_size & (section_size - 1)) == 0, \
-            'Number of memory blocks should be power of 2'
-
-        version = sim_input.get_template()
-        test_template = self.template + '/rv64-{}.S'.format(templates[version])
-
-        if intr: DINTR = ['-DINTERRUPT']
-        else: DINTR = []
-        extra_args = DINTR + [ '-I', '{}/include/p'.format(self.template) ]
-        if version in [ V_U ]:
-            rand = data[0] & 0xffffffff
-            extra_args = DINTR + [ '-DENTROPY=0x{:08x}'.format(rand), '-std=gnu99', '-O2',
-                                   '-I', '{}/include/v'.format(self.template),
-                                   '{}/include/v/string.c'.format(self.template),
-                                   '{}/include/v/vm.c'.format(self.template) ]
-
-        si_name = self.base + '/.input_{}.si'.format(self.proc_num)
-        asm_name = self.base + '/.input_{}.S'.format(self.proc_num)
-        elf_name = self.base + '/.input_{}.elf'.format(self.proc_num)
-        hex_name = self.base + '/.input_{}.hex'.format(self.proc_num)
-        sym_name = self.base + '/.input_{}.symbols'.format(self.proc_num)
-        rtl_intr_name = self.base + '/.input_{}.rtl.intr'.format(self.proc_num)
-        isa_intr_name = self.base + '/.input_{}.isa.intr'.format(self.proc_num)
-
+    def generate_assembly(self, template_lines, sim_input, data, num_data_sections, section_size):
         prefix_insts = sim_input.get_prefix()
         insts = sim_input.get_insts()
         suffix_insts = sim_input.get_suffix()
-        sim_input_ints = sim_input.ints.copy()
-
-        ints = []
-        for inst in insts[:-1]:
-            INT = sim_input_ints.pop(0)
-            if 'la' in inst:
-                ints.append(INT)
-                ints.append(0)
-            else:
-                ints.append(INT)
-
-        sim_input.save(si_name, data)
-
-        fd = open(test_template, 'r')
-        template_lines = fd.readlines()
-        fd.close()
 
         assembly = []
         for line in template_lines:
@@ -130,11 +87,9 @@ class rvPreProcessor():
 
                         assembly.append('{:<16}.dword 0x{:016x}, 0x{:016x}\n'.\
                                         format(label, data[i], data[i+1]))
+        return assembly
 
-        fd = open(asm_name, 'w')
-        fd.writelines(assembly)
-        fd.close()
-
+    def compile_asm(self, extra_args, asm_name, elf_name):
         cc_args = self.cc_args + extra_args + [ asm_name, '-o', elf_name ]
 
         cc_ret = -1
@@ -142,13 +97,78 @@ class rvPreProcessor():
             cc_ret = subprocess.call(cc_args)
             # if cc_ret == -9: cc process is killed by OS due to memory usage
             if cc_ret != -9: break
+        return cc_ret
 
-        if cc_ret == 0:
-            subprocess.call(cc_args)
+    def process(self, sim_input: simInput, data_a: list, data_b: list, intr: bool, num_data_sections=6): #TODO check this section, especially for data
+        section_size = len(data_a) // num_data_sections
 
-            elf2hex_args = self.elf2hex_args + [ elf_name, '--output', hex_name]
+        assert section_size == len(data_b), 'data sections have to be of equal length'
+        assert data_a, 'Empty data can not be processed'
+        assert (section_size & (section_size - 1)) == 0, \
+            'Number of memory blocks should be power of 2'
+
+        version = sim_input.get_template()
+        test_template = self.template + '/rv64-{}.S'.format(templates[version])
+
+        if intr: DINTR = ['-DINTERRUPT']
+        else: DINTR = []
+        extra_args = DINTR + [ '-I', '{}/include/p'.format(self.template) ]
+        if version in [ V_U ]:
+            rand = data_a[0] & 0xffffffff
+            extra_args = DINTR + [ '-DENTROPY=0x{:08x}'.format(rand), '-std=gnu99', '-O2',
+                                   '-I', '{}/include/v'.format(self.template),
+                                   '{}/include/v/string.c'.format(self.template),
+                                   '{}/include/v/vm.c'.format(self.template) ]
+
+        si_name_a = self.base + '/.input_{}_a.si'.format(self.proc_num)
+        si_name_b = self.base + '/.input_{}_b.si'.format(self.proc_num)
+        asm_name_a = self.base + '/.input_{}_a.S'.format(self.proc_num)
+        asm_name_b = self.base + '/.input_{}_b.S'.format(self.proc_num)
+        elf_name_a = self.base + '/.input_{}_a.elf'.format(self.proc_num)
+        elf_name_b = self.base + '/.input_{}_b.elf'.format(self.proc_num)
+        hex_name = self.base + '/.input_{}.hex'.format(self.proc_num)
+        sym_name = self.base + '/.input_{}.symbols'.format(self.proc_num)
+        rtl_intr_name = self.base + '/.input_{}.rtl.intr'.format(self.proc_num)
+        isa_intr_name = self.base + '/.input_{}.isa.intr'.format(self.proc_num)
+
+        sim_input_ints = sim_input.ints.copy()
+        insts = sim_input.get_insts()
+
+        ints = []
+        for inst in insts[:-1]:
+            INT = sim_input_ints.pop(0)
+            if 'la' in inst:
+                ints.append(INT)
+                ints.append(0)
+            else:
+                ints.append(INT)
+
+        sim_input.save(si_name_a, data_a)
+        sim_input.save(si_name_b, data_b)
+
+        fd = open(test_template, 'r')
+        template_lines = fd.readlines()
+        fd.close()
+
+        assembly_a = self.generate_assembly(template_lines, sim_input, data_a, num_data_sections, section_size)
+
+        assembly_b = self.generate_assembly(template_lines, sim_input, data_b, num_data_sections, section_size)
+
+        fd = open(asm_name_a, 'w')
+        fd.writelines(assembly_a)
+        fd.close()
+        fd = open(asm_name_b, 'w')
+        fd.writelines(assembly_b)
+        fd.close()
+
+        cc_ret_a = self.compile_asm(extra_args, asm_name_a, elf_name_a)
+        cc_ret_b = self.compile_asm(extra_args, asm_name_b, elf_name_b)
+        
+        if cc_ret_a == 0 and cc_ret_b == 0:
+
+            elf2hex_args = self.elf2hex_args + [ elf_name_a, '--output', hex_name]
             subprocess.call(elf2hex_args)
-            symbols= self.get_symbols(elf_name, sym_name)
+            symbols= self.get_symbols(elf_name_a, sym_name)
 
             if intr:
                 fuzz_main = symbols['_fuzz_main']
@@ -162,11 +182,11 @@ class rvPreProcessor():
             if version in [ V_U ]:
                 max_cycles = 200000
 
-            isa_input = isaInput(elf_name, isa_intr_name)
-            rtl_input = rtlInput(hex_name, rtl_intr_name, data, symbols, max_cycles)
+            hsc_input = hscInput(elf_name_a, elf_name_b, isa_intr_name)
+            rtl_input = rtlInput(hex_name, rtl_intr_name, data_a, data_b, symbols, max_cycles)
         else:
-            isa_input = None
+            hsc_input = None
             rtl_input = None
             symbols = None
 
-        return (isa_input, rtl_input, symbols)
+        return (hsc_input, rtl_input, symbols)
